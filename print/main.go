8b018a5736090e50fd1b31f803ae2568c4513fbc
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-30 04:10:04 +0530

    updated

---
+++ print/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 22
+	name := "Dwaipayan"
+	height := 5.8
+
+	fmt.Println("age:",age, "height:",height, "name:",name)
+
+	fmt.Println("Dwaipayan Biswas")
+
+	fmt.Printf("Hello\n")
+	fmt.Printf("height: %f\n",height)
+	fmt.Printf("height: %T\n",height)
+
+
+}
