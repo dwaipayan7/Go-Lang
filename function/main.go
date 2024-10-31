8b9359cbe07f793caf64f77dcd5562730d3f11b6
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-31 05:47:38 +0530

    update

--- function/main.go
+++ function/main.go
@@ -11,10 +11,19 @@ func add(a, b int) (result int) {
 	return
 }
 
+func multiply(a , b int) (result int){
+	result = a * b
+	return
+}
+
 func main() {
 	fmt.Println("Functions in Go")
 	simpleFunction()
 
 	ans := add(3, 4)
 	fmt.Println(ans)
+
+	mul :=  multiply(5, 6)
+	fmt.Println(mul)
+
 }
