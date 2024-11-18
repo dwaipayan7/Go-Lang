2432c13d3cf2f6202ba1291f1d813d0798f7a93e
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-18 16:39:24 +0530

    updated

--- crud/main.go
+++ crud/main.go
@@ -1,30 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-
 )
+
+type Todo struct {
+	UserID    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
 
 func main() {
 	fmt.Println("Learning CRUD in GO")
 
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos")
 	if err != nil {
-	fmt.Printf("Getting Error\n")
-	return
-	}
-	defer res.Body.Close()
-
-	data,err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
 		fmt.Printf("Getting Error\n")
 		return
 	}
+	defer res.Body.Close()
 
-	fmt.Println("Data: ",string(data))
+	// data, err := ioutil.ReadAll(res.Body)
 
+	// if err != nil {
+	// 	fmt.Printf("Getting Error\n")
+	// 	return
+	// }
+
+	// fmt.Println("Data: ", string(data))
+
+	var todo Todo
+	json.NewDecoder(res.Body).Decode(&todo)
+
+	fmt.Println("Todo: ", todo)
+
+	fmt.Println("Completed response: ",todo.Completed);
 
 }
