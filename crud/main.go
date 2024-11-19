e918abc077177a6153340cef950f72c5f0c63159
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-11-19 10:23:21 +0530

    updated

--- crud/main.go
+++ crud/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type Todo struct {
@@ -13,8 +15,7 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
-func main() {
-	fmt.Println("Learning CRUD in GO")
+func performGetRequest() {
 
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos")
 	if err != nil {
@@ -37,6 +38,48 @@ func main() {
 
 	fmt.Println("Todo: ", todo)
 
-	fmt.Println("Completed response: ",todo.Completed);
+	fmt.Println("Completed response: ", todo.Completed)
+
+}
+
+func performPostRequest(){
+	 todo := Todo{
+		UserID: 22,
+		Title: "Dwaipayan",
+		Completed: true,
+	}
+
+	jsonData, err :=json.Marshal(todo)
+
+	if err != nil {
+		fmt.Println("Error Marshalling",err)
+		return
+	}
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos"
+
+	jsonString := string(jsonData)
+
+	jsonReader := strings.NewReader(jsonString)
+
+	res, err :=	http.Post(myUrl, "application/json", jsonReader)
+
+	if err != nil {
+		fmt.Println("Getting error",err)
+	}
+	defer res.Body.Close()
+
+	data,_ := ioutil.ReadAll(res.Body)
+	fmt.Println("Data: ", string(data))
+
+
+}
+
+func main() {
+	fmt.Println("Learning CRUD in GO")
+
+	performGetRequest()
+
+	performPostRequest()
 
 }
